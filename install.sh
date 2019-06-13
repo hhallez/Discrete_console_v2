69cf13f308e19923688dc7d1228b7285b44e473d
Hans Hallez <hans.hallez@kuleuven.be>
2019-06-13 14:24:00 +0200

    updated install.sh

--- install.sh
+++ install.sh
@@ -3,14 +3,17 @@ SERVICE=Discrete;
 
 if ps ax | grep -v grep | grep $SERVICE > /dev/null
 then
-    echo "$SERVICE service running, everything is fine"
+    echo "$SERVICE service running, stopping service"
     sudo systemctl stop $SERVICE.service
     sudo systemctl disable $SERVICE.service
+    echo "$SERVICE service stopped, updating..."
     sudo cp $SERVICE.service /etc/systemd/system/$SERVICE.service
     sudo systemctl start Discrete.service
     sudo systemctl enable Discrete.service
 else
+  echo "installing $SERVICE service..."
   sudo cp $SERVICE.service /etc/systemd/system/$SERVICE.service
   sudo systemctl start Discrete.service
   sudo systemctl enable Discrete.service
 fi
+echo "done"
